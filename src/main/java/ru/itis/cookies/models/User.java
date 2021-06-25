f2ed1285a40f10078e4e6785b2ea557b6db7b3be
aallaguly01 <aallaguly01@gmail.com>
2021-06-25 16:28:53 +0300

    add demo model User

---
+++ src/main/java/ru/itis/cookies/models/User.java
@@ -0,0 +1,29 @@
+package ru.itis.cookies.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "account")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    public enum Role{
+        WORKER, STEWARD, ADMIN
+    }
+}
