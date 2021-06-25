4b1c67da04c8ea27a94f3a6c2cdf59350233311e
aallaguly01 <aallaguly01@gmail.com>
2021-06-25 16:28:13 +0300

    add frame

--- Tables/src/InventoryT.java
+++
@@ -1,9 +0,0 @@
-public class InventoryT {
-    int productID;
-    int counter;
-
-    public InventoryT(int productID, int counter) {
-        this.productID = productID;
-        this.counter = counter;
-    }
-}
